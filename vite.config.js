b179d1bffc833c926b9628de60bdce688f745ebd
Julián Liberatore <julianLib1997@gmail.com>
2025-08-28 17:02:36 +0200

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env
+  },    
   plugins: [vue()],
   resolve: {
     alias: [
       {
         find: /^~.+/,
         replacement: (val) => {
-          return val.replace(/^~/, "")
+          return val.replace(/^~/, "");
         },
       },
     ],
   },
-  base: '/', 
-
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        404: resolve(__dirname, 'index.html')
-      }
-    }
-  }
 })
